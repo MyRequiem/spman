7c14def063a39853cbde95472b2738039c21a81a
MyRequiem <mailto:mrvladislavovich@gmail.com>
2018-04-20 11:45:16 +0400

    Changed the license from 'GNU GENERAL PUBLIC LICENSE Version 3' to MIT

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,9 @@
-# spman
+# __init_.py file is part of spman
+#
+# spman - Slackware package manager
+# Home page: https://github.com/MyRequiem/spman
+#
+# Copyright (c) 2018 Vladimir MyRequiem Astrakhan, Russia
+# <mrvladislavovich@gmail.com>
+# All rights reserved
+# See LICENSE for details.
--- src/pkglist.py
+++ src/pkglist.py
@@ -2,23 +2,15 @@
 # -*- coding: utf-8 -*-
 
 # pkglist.py file is part of spman
-
-# Copyright 2016 MyRequiem <mrvladislavovich@gmail.com>
-# All rights reserved
-
+#
 # spman - Slackware package manager
 # Home page: https://github.com/MyRequiem/spman
+#
+# Copyright (c) 2018 Vladimir MyRequiem Astrakhan, Russia
+# <mrvladislavovich@gmail.com>
+# All rights reserved
+# See LICENSE for details.
 
-# Spman is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-# You should have received a copy of the GNU General Public License
-# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
 pkglist.py

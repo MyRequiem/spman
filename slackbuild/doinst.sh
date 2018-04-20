a16594e101d4ae7acf8ec3715d68194f85ca8c0b
MyRequiem <mailto:mrvladislavovich@gmail.com>
2018-04-20 17:08:21 +0400

    update slackbuild directory (README, doinst.sh, slack-desc, spman.SlackBuild)

--- slackbuild/doinst.sh
+++ slackbuild/doinst.sh
@@ -1,21 +1,15 @@
-#!/bin/bash
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    rm $NEW
+  fi
+}
 
-CONFIGS="\
-    blacklist \
-    repo-list \
-    spman.conf \
-"
+CONFIGS="blacklist repo-list spman.conf"
 
-for CONFIG in ${CONFIGS}; do
-    OLD="/etc/spman/${CONFIG}"
-    NEW="${OLD}.new"
-    if ! [ -r "${OLD}" ]; then
-        mv "${NEW}" "${OLD}"
-    else
-        MD5OLD=$(md5sum "${OLD}" | cut -d " " -f 1)
-        MD5NEW=$(md5sum "${NEW}" | cut -d " " -f 1)
-        if [[ "x${MD5OLD}" == "x${MD5NEW}" ]]; then
-            rm "${NEW}"
-        fi
-    fi
+for file in $CONFIGS; do
+    config etc/spman/${file}.new
 done

1052262b112be87e9dd064dbd240cc87960f1342
MyRequiem <mailto:mrvladislavovich@gmail.com>
2018-04-23 15:14:52 +0400

    modified changeVersion.sh

--- changeVersion.sh
+++ changeVersion.sh
@@ -1,14 +1,14 @@
 #! /bin/sh
 
 VERSION=$(grep "self.prog_version = " src/maindata.py | cut -d \' -f 2)
-TMP=tmp
 INFILE="
     INSTALL
     README.md
+    slackbuild/spman.info
 "
 
 for FILE in ${INFILE}; do
-    sed "s/[0-9]\\.[0-9]\\.[0-9]/${VERSION}/g" "${FILE}" > ${TMP}
+    sed "s/[0-9]\\.[0-9]\\.[0-9]/${VERSION}/g" "${FILE}" > "${FILE}_"
     rm -f "${FILE}"
-    mv ${TMP} "${FILE}"
+    mv "${FILE}_" "${FILE}"
 done

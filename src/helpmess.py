d5c5fc5643a5fec4d6b7dacb42d645aec90b0027
MyRequiem <mailto:mrvladislavovich@gmail.com>
2018-04-23 13:39:53 +0400

    update 'spman --help'  output

--- src/helpmess.py
+++ src/helpmess.py
@@ -20,57 +20,71 @@ from .maindata import MainData
 
 
 def show_help_mess(repo: str = '') -> None:
-    """{0}Version: {1}{6}
+    """{0}Version: {1}
+Home page: {4}
+{3}{5}
+
 Usage: spman command [param[, param ...]]
 
-{2} -h, --help{3}            Print this help message and exit
-{2} -v, --check-version{3}   Check program version for update
-{2} -l, --repolist{3}        Print a list of all the repositories
-{2} -r, --repoinfo{3}        Show information about all active repositories
-{2} -b, --blacklist{3}       Show blacklist
-{2} -u, --update{3}          Update local data for all repositories
-{2} -t, --health{3}          Check health installed packages
-{2} -w, --new-config{3}      Search for *.new config files on the system
-{2} -g, --check-upgrade{3}   Check all installed packages for upgrade
-{2} -d, --download{3}        Download binary package(s) or source code
-{2}     --pkg|--src{3}           for reposytory 'multilib' only --pkg
-                           for reposytory 'sbo' only --src
-{2}     reponame{3}              name of repository
-{2}     pkg1[ pkg2...]{3}        list of packages name
-{2} -q, --queue{3}           Download, build and install package(s) in the
-                         queue from 'sbo' repository
-{2}     --add pkglist{3}         add package(s) to the queue
-{2}     --remove{3}              remove package(s) from the queue
-{2}     --clear{3}               clear queue
-{2}     --show{3}                print queue
-{2}     --install{3}             download, build and install package(s)
-{2} -p, --find-deps pkg{3}   Show list all dependencies for package from 'sbo'
-                         repository
-{2} -s, --view-slackbuild{3} View the contents of files included in SlackBuild
-                         archive using pager ($PAGER)
-{2}         pkg{3}               package name
-{2} -f, --find-pkg pkg{3}    Search package from each enabled repository and
-                         view info
-{2} -i, --pkglist{3}         Show complete list of the packages in the repository
-{2}     reponame{3}              name of repository
-{2}     --only-installed{3}      output only installed packages
-{2} -k, --check-deps{3}      Search dependency problems in the system packages
-{2}     --sbbdep{3}              using \'sbbdep\' tool
-{2}     --ldd{3}                 using \'ldd\' tool
-{2} -a, --bad-links{3}       Search for links to non-existent files/dirs
-{2}     /path/to/dir{3}          directory for searching
-
-{0}Home page: {5}
-{4}{6}"""
+{2} -h, --help{5}
+    Print this help message and exit
+{2} -v, --check-version{5}
+    Check program version for update
+{2} -l, --repolist{5}
+    Print a list of all the repositories
+{2} -r, --repoinfo{5}
+    Show information about all active repositories
+{2} -b, --blacklist{5}
+    Show blacklist
+{2} -u, --update{5}
+    Update local data for all repositories
+{2} -t, --health{5}
+    Check health installed packages
+{2} -w, --new-config{5}
+    Search for *.new config files on the system
+{2} -g, --check-upgrade{5}
+    Check all installed packages for upgrade
+{2} -d, --download --pkg|--src reponame pkg1[ pkg2...]{5}
+    Download binary package(s) or source code from specified repository
+{6}    Note:{5} for reposytory 'multilib' only --pkg
+          for reposytory 'sbo' only --src
+{2} -q, --queue --add pkglist|--remove pkglist|--clear|--show|--install{5}
+    Download, build and install package(s) in the queue from SlackBuilds.org
+{6}    Note:{5} pkglist - list of names of packages
+      --add pkglist{5}      add package(s) to the queue
+      --remove pkglist{5}   remove package(s) from the queue
+      --clear{5}            clear queue
+      --show{5}             print queue
+      --install{5}          download, build and install package(s)
+{2} -p, --find-deps pkgname{5}
+    Show list all dependencies for package from 'sbo' repository. Installed
+    packages are highlighted in green.
+{2} -s, --view-slackbuild pkgname{5}
+    View the contents of files included in SlackBuild archive using pager
+{2} -f, --find-pkg [--strict] pkgname{5}
+    Search package from each enabled repository and view info
+    (case-insensitive)
+      --strict    - strict match by package name
+{2} -i, --pkglist reponame [--only-installed]{5}
+    Show complete list of the packages on repository. Installed packages are
+    highlighted in green.
+      --only-installed      output only installed packages
+{2} -k, --check-deps --sbbdep|--ldd{5}
+    Search dependency problems in the system packages
+      --sbbdep      using \'sbbdep\' tool
+      --ldd         using \'ldd\' tool
+{2} -a, --bad-links /path/to/dir{5}
+    Search for links to nonexistent files/dir in the specified directory.
+"""
 
     meta = MainData()
 
     if repo == 'error':
         # parameter input error
-        print(('{0}Wrong parameters. '
-               '{2}Type {1}spman --help{2}').format(meta.clrs['red'],
-                                                    meta.clrs['cyan'],
-                                                    meta.clrs['reset']))
+        print(('{0}Wrong parameters {2}({1}'
+               'spman --help{2} for help)').format(meta.clrs['red'],
+                                                   meta.clrs['cyan'],
+                                                   meta.clrs['reset']))
         raise SystemExit
     elif repo:
         # repository does not exist or not available
@@ -84,8 +98,8 @@ Usage: spman command [param[, param ...]]
     # show usage
     print(str(show_help_mess.__doc__).format(meta.clrs['grey'],
                                              meta.prog_version,
-                                             meta.clrs['yellow'],
                                              meta.clrs['cyan'],
                                              meta.mail,
                                              meta.home_page,
-                                             meta.clrs['reset']))
+                                             meta.clrs['reset'],
+                                             meta.clrs['yellow']))

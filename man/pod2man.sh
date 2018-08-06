e7edaa56b170f71cac47e414465b4f9f35ea6b29
MyRequiem <mailto:mrvladislavovich@gmail.com>
2018-08-06 18:35:09 +0400

    man/pod2man.sh modified

--- man/pod2man.sh
+++ man/pod2man.sh
@@ -2,11 +2,14 @@
 
 PRGNAME="spman"
 MANSECTION="8"
+
+UPPERPPRGNAME=$(echo ${PRGNAME} | tr "[:lower:]" "[:upper:]")
 PODFILENAME="${PRGNAME}.pod"
 OUTPUT="${PRGNAME}.${MANSECTION}"
 
 pod2man "${PODFILENAME}" > "${OUTPUT}"
-sed -i "s/^\\.IX Title .*/\\.IX Title \"SPMAN 8\"/" "${OUTPUT}"
-sed -i "s/^\\.TH.*/\\.TH \"SPMAN\" \"8\" \"$(date +%Y-%m-%d)\" \"System\" \
-\"Linux User Manual\"/" "${OUTPUT}"
+sed -i "s/^\\.IX Title .*/\\.IX Title \"${UPPERPPRGNAME} ${MANSECTION}\"/" \
+    "${OUTPUT}"
+sed -i "s/^\\.TH.*/\\.TH \"${UPPERPPRGNAME}\" \"${MANSECTION}\" \
+\"$(date +%Y-%m-%d)\" \"System\" \"Linux User Manual\"/" "${OUTPUT}"
 nroff -man "${OUTPUT}" | less

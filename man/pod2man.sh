c48f159a2476b9caaa6e43b64a8eca930c8d3cb5
MyRequiem <mailto:mrvladislavovich@gmail.com>
2018-08-06 18:18:37 +0400

    man/spman.8 updated

--- man/pod2man.sh
+++ man/pod2man.sh
@@ -6,7 +6,7 @@ PODFILENAME="${PRGNAME}.pod"
 OUTPUT="${PRGNAME}.${MANSECTION}"
 
 pod2man "${PODFILENAME}" > "${OUTPUT}"
-sed -i 's/^\\.IX.*/\\.IX Title "SPMAN 8"/' "${OUTPUT}"
+sed -i "s/^\\.IX Title .*/\\.IX Title \"SPMAN 8\"/" "${OUTPUT}"
 sed -i "s/^\\.TH.*/\\.TH \"SPMAN\" \"8\" \"$(date +%Y-%m-%d)\" \"System\" \
 \"Linux User Manual\"/" "${OUTPUT}"
 nroff -man "${OUTPUT}" | less
